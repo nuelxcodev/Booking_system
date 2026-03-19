b796975650708496bb13994af9ab4d3956d4d95a
nuelxcodev <144784983+nuelxcodev@users.noreply.github.com>
2026-03-19 22:14:47 +0100

    Create uploadfile.js

---
+++ src/controller/uploadfile.js
@@ -0,0 +1,9 @@
+exports.uploadImage = (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  res.status(200).json({
+    url: `/uploads/${req.file.filename}`,
+  });
+};
