53aa3b878f16fe8df8f3371d4810cc1de7f5f280
nuelxcodev <144784983+nuelxcodev@users.noreply.github.com>
2026-03-19 22:15:37 +0100

    Create fileupload.js

---
+++ src/routes/fileupload.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const { uploadImage } = require("../controllers/upload.controller");
+
+const router = express.Router();
+
+// Storage config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  },
+});
+
+// File filter (only images)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images allowed"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+router.post("/upload-image", upload.single("image"), uploadImage);
+
+module.exports = router;
