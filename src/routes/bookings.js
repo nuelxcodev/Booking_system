be7f360611f4419ca89ad9f2b5bf8222f524d87d
nuelxcodev <144784983+nuelxcodev@users.noreply.github.com>
2026-03-19 22:03:29 +0100

    Update bookings.js

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const {
+  bookRoom,
+  getBookings,
+} = require("../controllers/booking.controller");
 
+const router = express.Router();
+
+router.post("/book-room", bookRoom);
+router.get("/bookings", getBookings);
+
+module.exports = router;
